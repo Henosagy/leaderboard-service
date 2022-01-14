dc4c76ebeb1b3fe3040120e65a225f6c594893a6
Henry Osei Agyemang <91277878+Henosagy@users.noreply.github.com>
2022-01-14 15:47:23 +0000

    Implementation of Profile Test

--- src/test/java/io/turntabl/leaderboardservice/model/ProfileTest.java
+++ src/test/java/io/turntabl/leaderboardservice/model/ProfileTest.java
@@ -7,13 +7,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProfileTest {
 
-
+    @Mock
     private Profile profile;
 
     @Test
@@ -21,7 +23,10 @@ class ProfileTest {
         LanguageLevel level1 = mock(LanguageLevel.class);
         LanguageLevel level2 = mock(LanguageLevel.class);
 
-//        List<LanguageLevel>
+        List<LanguageLevel> languageLevels = List.of(level1, level2);
 
+        when(profile.setLanguageLevels(languageLevels)).thenReturn(profile);
+
+        assertThat(profile.setLanguageLevels(languageLevels)).isEqualTo(profile);
     }
 }
